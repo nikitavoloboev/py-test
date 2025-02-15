99d1a69c2e644ef3343c47eeebb531cfa2c669d4
Nikita <github@nikiv.dev>
2025-02-15 02:27:49 +0100

    .

--- scripts/pydantic_ai.py
+++ scripts/pydantic_ai.py
@@ -1,27 +1,26 @@
-# TODO: get it working
-# import os
-# from typing import cast
+import os
+from typing import cast
 
-# import logfire
-# from pydantic import BaseModel
+import logfire
+from pydantic import BaseModel
 
-# from pydantic_ai import Agent
-# from pydantic_ai.models import KnownModelName
+from pydantic_ai import Agent
+from pydantic_ai.models import KnownModelName
 
-# # 'if-token-present' means nothing will be sent (and the example will work) if you don't have logfire configured
-# logfire.configure(send_to_logfire="if-token-present")
-
-
-# class MyModel(BaseModel):
-#     city: str
-#     country: str
+# 'if-token-present' means nothing will be sent (and the example will work) if you don't have logfire configured
+logfire.configure(send_to_logfire="if-token-present")
 
 
-# model = cast(KnownModelName, os.getenv("PYDANTIC_AI_MODEL", "openai:gpt-4o"))
-# print(f"Using model: {model}")
-# agent = Agent(model, result_type=MyModel)
+class MyModel(BaseModel):
+    city: str
+    country: str
 
-# if __name__ == "__main__":
-#     result = agent.run_sync("The windy city in the US of A.")
-#     print(result.data)
-#     print(result.usage())
+
+model = cast(KnownModelName, os.getenv("PYDANTIC_AI_MODEL", "openai:gpt-4o"))
+print(f"Using model: {model}")
+agent = Agent(model, result_type=MyModel)
+
+if __name__ == "__main__":
+    result = agent.run_sync("The windy city in the US of A.")
+    print(result.data)
+    print(result.usage())
